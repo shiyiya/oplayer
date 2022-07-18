bf3d3a70e737414287ad80350d028601d7fe305a
CY <shiyiya.11@gmail.com>
2022-07-18 02:44:21 +0000

    .

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,12 @@ export const rollupPlugins: Plugin[] = []
 
 const globals = {
   '@oplayer/core': 'OPlayer',
+  '@oplayer/ui': 'OUI',
   '@oplayer/hls': 'OHls',
   'hls.js/dist/hls.light.min.js': 'Hls',
   '@oplayer/torrent': 'OTorrent',
   'webtorrent/webtorrent.min.js': 'WebTorrent',
-  react: 'react'
+  react: 'React'
 }
 
 export const external = Object.keys(globals)
