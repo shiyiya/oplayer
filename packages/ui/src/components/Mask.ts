cd54e4356d8e97d1951c9d89c0137c11cfb6edbe
shiyiya <shiyiya.11@gmail.com>
2022-09-15 22:02:40 +0800

    .

--- packages/ui/src/components/Mask.ts
+++ packages/ui/src/components/Mask.ts
@@ -4,14 +4,22 @@ import initListener from '../listeners/init'
 import { settingShown } from '../style'
 import { hasClass } from '../utils'
 
+export const maskCls = $.css({
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  right: '0',
+  bottom: '0',
+
+  [`@global .${settingShown} &`]: {
+    'z-index': '98'
+  }
+})
+
 const render = (player: Player, el: HTMLElement) => {
-  const $dom = $.create(
-    `div.${$.css`
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      inset: 0;`}`
-  )
+  const $dom = $.create(`div.${maskCls}`)
 
   $dom.addEventListener('click', () => {
     if (!initListener.isInitialized() || hasClass(player.$root, settingShown)) return
