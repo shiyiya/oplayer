ae0f28b830e0190849e76a4441c7ee52b965c85f
CY <shiyiya.11@gmail.com>
2022-11-28 15:45:01 +0800

    Update example

--- examples/standalone/main.ts
+++ examples/standalone/main.ts
@@ -18,6 +18,7 @@ import { html, render } from 'lit'
 import { live } from 'lit/directives/live.js'
 import { ref } from 'lit/directives/ref.js'
 import { played } from '@oplayer/ui/src/components/Progress.style'
+import { $ } from '@oplayer/core'
 
 const dataSrcs = [
   MP4,
@@ -95,7 +96,19 @@ const player = Player.make(document.getElementById('player')!, {
       ],
       menu: [
         {
+          name: 'airplay',
+          icon: `<svg style='scale:0.9' viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M662.186667 981.333333H361.813333c-17.194667 0-32.853333-9.386667-40.661333-23.552a50.176 50.176 0 0 1 0-47.104l150.186667-260.565333c7.808-14.08 23.466667-23.509333 40.661333-23.509333 17.194667 0 32.853333 9.386667 40.661333 23.552l150.186667 260.565333c7.850667 14.08 7.850667 32.938667 0 47.061333-7.808 14.122667-23.466667 23.552-40.661333 23.552z m-219.008-94.165333h137.642666L512 767.872l-68.821333 119.296z"></path><path d="M821.76 841.642667h-100.138667c-26.581333 0-46.933333-20.437333-46.933333-47.104 0-26.666667 20.352-47.104 46.933333-47.104h100.138667c37.546667 0 67.285333-29.824 67.285333-67.498667V204.373333c-1.578667-37.674667-31.317333-67.498667-67.285333-67.498666H203.818667c-37.546667 0-67.285333 29.866667-67.285334 67.498666v477.184c0 37.674667 29.738667 67.498667 67.285334 67.498667h100.096c26.624 0 46.933333 20.394667 46.933333 47.104 0 26.666667-20.309333 47.104-46.933333 47.104H203.818667A163.541333 163.541333 0 0 1 42.666667 679.893333V204.373333A161.194667 161.194667 0 0 1 203.818667 42.666667H821.76C909.354667 42.666667 981.333333 114.858667 981.333333 204.373333v477.141334c0 87.893333-71.978667 160.128-159.573333 160.128z"></path></svg>`,
+          onClick: () => {
+            if (window.WebKitPlaybackTargetAvailabilityEvent) {
+              player.$video.webkitShowPlaybackTargetPicker()
+            } else {
+              player.plugins.ui.notice('Airplay not available')
+            }
+          }
+        },
+        {
           name: '视频列表',
+          icon: `<svg style='scale:1.2' viewBox="0 0 1032 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6368" width="200" height="200"><path d="M192.031 452c-33.188 0-60 26.813-60 60s26.813 60 60 60 60-26.813 60-60-26.813-60-60-60zM192.031 212c-33.188 0-60 26.813-60 60s26.813 60 60 60 60-26.813 60-60-26.813-60-60-60zM192.031 698.75c-29.625 0-53.156 24-53.156 53.156s24 53.156 53.156 53.156 53.156-24 53.156-53.156c0-29.156-23.625-53.156-53.156-53.156zM312.031 791.938h559.969v-79.969h-559.969v79.969zM312.031 552.031h559.969v-79.969h-559.969v79.969zM312.031 231.969v79.969h559.969v-79.969h-559.969z" fill="#ffffff" p-id="6369"></path></svg>`,
           children: [
             {
               name: 'mp4',
@@ -145,6 +158,98 @@ const player = Player.make(document.getElementById('player')!, {
           console.log('custom plugin')
         }
       })
+    },
+    {
+      name: 'oplayer-plugin-chromecast',
+      apply(player) {
+        let cast, session, currentMedia
+
+        function loadChromecast() {
+          return new Promise((resolve, reject) => {
+            $.render(
+              $.create('script', {
+                type: 'text/javascript',
+                src: 'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1'
+              }),
+              document.body
+            )
+
+            window.__onGCastApiAvailable = (isAvailable) => {
+              if (isAvailable) {
+                cast = window.chrome.cast
+                const sessionRequest = new cast.SessionRequest(
+                  cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
+                )
+                const apiConfig = new cast.ApiConfig(
+                  sessionRequest,
+                  () => {},
+                  (status) => {
+                    if (status === cast.ReceiverAvailability.AVAILABLE) {
+                    } else {
+                    }
+                  }
+                )
+                cast.initialize(apiConfig, resolve, reject)
+              } else {
+                player.plugins.ui.notice('Chromecast not available')
+                player.plugins.ui.menu.unregister('chromecast')
+                reject()
+              }
+            }
+          })
+        }
+
+        const discoverDevices = () => {
+          cast.requestSession(
+            (session) => {
+              session = session
+              const mediaInfo = new cast.media.MediaInfo(player.options.video.url)
+              const request = new cast.media.LoadRequest(mediaInfo)
+
+              if (!session) window.open(media)
+
+              session
+                .loadMedia(
+                  request,
+                  (media) => {
+                    currentMedia = media
+                  },
+                  (err) => {
+                    player.plugins.ui.notice('Chromecast: ' + err.message)
+                  }
+                )
+                .play()
+            },
+            (err) => {
+              if (err.code === 'cancel') {
+                session = undefined
+              } else {
+                player.plugins.ui.notice('Chromecast: ' + err.code + (err.description || ''))
+              }
+            }
+          )
+        }
+
+        player.plugins.ui.menu.register({
+          name: 'chromecast',
+          icon: `<svg style='scale:0.9' viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2081" width="200" height="200"><path d="M895.66 128H128a85.44 85.44 0 0 0-85.44 85.44v127.84H128v-127.84h767.66v597.12H597.28V896H896a85.44 85.44 0 0 0 85.44-85.44V213.44A85.44 85.44 0 0 0 896 128zM42.56 767.16v127.84h127.82a127.82 127.82 0 0 0-127.82-127.84z m0-170.56V682a213.26 213.26 0 0 1 213.28 213.32v0.68h85.44a298.38 298.38 0 0 0-298-298.72h-0.66z m0-170.54v85.44c212-0.2 384 171.5 384.16 383.5v1h85.44c-0.92-258.92-210.68-468.54-469.6-469.28z"></path></svg>`,
+          onClick() {
+            let promis = Promise.resolve()
+            if (!cast) {
+              promis = loadChromecast()
+            }
+
+            promis.then((_) => {
+              if (session) {
+                currentMedia?.stop()
+                session?.stop()
+              } else {
+                discoverDevices()
+              }
+            })
+          }
+        })
+      }
     },
     // ad({
     //   autoplay: false,
