c11782e858e93288697f871bb3afd4d2d4d9f8f5
shiyiya <shiyiya.11@gmail.com>
2022-12-25 13:30:15 +0800

    -

--- packages/dash/src/index.ts
+++ packages/dash/src/index.ts
@@ -5,8 +5,6 @@ const PLUGIN_NAME = 'oplayer-plugin-dash'
 
 let imported: typeof import('dashjs') = globalThis.dashjs
 
-const languageIcon = `<svg viewBox="0 0 1024 1024"><path d="M512 85.333333C277.333333 85.333333 85.333333 277.333333 85.333333 512s192 426.666667 426.666667 426.666667 426.666667-192 426.666667-426.666667S746.666667 85.333333 512 85.333333z m294.4 256H682.666667c-12.8-55.466667-34.133333-102.4-59.733334-153.6 76.8 29.866667 145.066667 81.066667 183.466667 153.6zM512 170.666667c34.133333 51.2 64 106.666667 81.066667 170.666666h-162.133334c17.066667-59.733333 46.933333-119.466667 81.066667-170.666666zM183.466667 597.333333c-8.533333-25.6-12.8-55.466667-12.8-85.333333s4.266667-59.733333 12.8-85.333333h145.066666c-4.266667 29.866667-4.266667 55.466667-4.266666 85.333333s4.266667 55.466667 4.266666 85.333333H183.466667z m34.133333 85.333334H341.333333c12.8 55.466667 34.133333 102.4 59.733334 153.6-76.8-29.866667-145.066667-81.066667-183.466667-153.6zM341.333333 341.333333H217.6c42.666667-72.533333 106.666667-123.733333 183.466667-153.6C375.466667 238.933333 354.133333 285.866667 341.333333 341.333333z m170.666667 512c-34.133333-51.2-64-106.666667-81.066667-170.666666h162.133334c-17.066667 59.733333-46.933333 119.466667-81.066667 170.666666z m98.133333-256H413.866667c-4.266667-29.866667-8.533333-55.466667-8.533334-85.333333s4.266667-55.466667 8.533334-85.333333h200.533333c4.266667 29.866667 8.533333 55.466667 8.533333 85.333333s-8.533333 55.466667-12.8 85.333333z m12.8 238.933334c25.6-46.933333 46.933333-98.133333 59.733334-153.6h123.733333c-38.4 72.533333-106.666667 123.733333-183.466667 153.6z m76.8-238.933334c4.266667-29.866667 4.266667-55.466667 4.266667-85.333333s-4.266667-55.466667-4.266667-85.333333h145.066667c8.533333 25.6 12.8 55.466667 12.8 85.333333s-4.266667 59.733333-12.8 85.333333h-145.066667z"></path></svg>`
-
 type PluginOptions = {
   config?: MediaPlayerSettingClass
   matcher?: (video: HTMLVideoElement, source: Source) => boolean
@@ -31,6 +29,12 @@ type Options = {
    */
   withBitrate?: boolean
 }
+
+const defaultMatcher: PluginOptions['matcher'] = (_, source) =>
+  source.format === 'dash' ||
+  source.format === 'mpd' ||
+  ((source.format === 'auto' || typeof source.format === 'undefined') &&
+    /.mpd(#|\?|$)/i.test(source.src))
 
 interface SettingItem {
   name: string
@@ -64,11 +68,6 @@ function getSettingsByType(instance: MediaPlayerClass, type: 'video', withBitrat
   return []
 }
 
-const defaultMatcher: PluginOptions['matcher'] = (_, source) =>
-  source.format === 'mpd' ||
-  ((source.format === 'auto' || typeof source.format === 'undefined') &&
-    /.mpd(#|\?|$)/i.test(source.src))
-
 const generateSetting = (player: Player, instance: MediaPlayerClass, options: Options) => {
   if (options.qualityControl) {
     instance.on(imported.MediaPlayer.events.STREAM_INITIALIZED, function () {
@@ -101,7 +100,7 @@ const generateSetting = (player: Player, instance: MediaPlayerClass, options: Op
       if (options.audioControl) {
         settingUpdater({
           name: player.locales.get('Language'),
-          icon: languageIcon,
+          icon: player.plugins.ui.icons.lang,
           settings() {
             return instance.getTracksFor('audio').map<SettingItem>((it) => ({
               name: it.lang || 'unknown',
@@ -207,6 +206,7 @@ const plugin = ({
         removeSetting(player)
         instance.destroy()
         instance = null
+        player.loader = null
       }
 
       if (options.loader || !isMatch) return false
@@ -218,6 +218,7 @@ const plugin = ({
       instance = imported.MediaPlayer().create()
       if (config) instance.updateSettings(config)
       instance.initialize(player.$video, source.src, player.$video.autoplay)
+      player.loader = instance
 
       if (!player.isNativeUI && player.plugins.ui?.setting) {
         generateSetting(player, instance, {
@@ -243,12 +244,11 @@ const plugin = ({
         instance = null
       })
 
-      return {
-        value: () => instance,
-        constructor: () => imported
-      }
+      return () => imported
     }
   }
 }
+
+plugin.prototype.defaultMatcher = defaultMatcher
 
 export default plugin
