d059064ebe528b7d714781e3adb44bad053715da
shiyiya <shiyiya.11@gmail.com>
2022-12-28 20:33:30 +0800

    slid left or right to seek

--- packages/ui/src/components/Mask.ts
+++ packages/ui/src/components/Mask.ts
@@ -1,7 +1,7 @@
 import type Player from '@oplayer/core'
 import { $, isMobile } from '@oplayer/core'
 import { settingShown } from '../style'
-import { hasClass } from '../utils'
+import { formatTime, hasClass } from '../utils'
 
 export const maskCls = $.css({
   width: '100%',
@@ -43,7 +43,58 @@ const render = (player: Player, el: HTMLElement, toggleController: Function) => 
     }, 200)
   })
 
+  if (isMobile) {
+    let touchedTime = 0
+    let touchStartPointerX = 0
+    let shouldSeekSec = 0
+    const rect = player.$root.getBoundingClientRect()
+    $dom.oncontextmenu = () => false
+
+    const moving = (e: TouchEvent) => {
+      e.preventDefault()
+      const x = (<TouchEvent>e).changedTouches[0]!.clientX - rect.left
+      // const y = (<TouchEvent>e).changedTouches[0]!.clientX - rect.top
+      const distance = x - touchStartPointerX
+      const distancePercent = distance / rect.width
+      shouldSeekSec = 60 * distancePercent
+
+      player.emit('notice', {
+        text: `${formatTime(
+          minmax(player.currentTime + shouldSeekSec, [0, player.duration])
+        )} / ${formatTime(player.duration)}`
+      })
+    }
+
+    $dom.addEventListener('touchstart', (e) => {
+      touchStartPointerX = (<TouchEvent>e).changedTouches[0]!.clientX
+      const id = setInterval(() => {
+        touchedTime += 100
+        if (touchedTime >= 1000) {
+          clearInterval(id)
+          //左右滑动可以调整进度
+          player.emit('notice', { text: 'slid left or right to seek' })
+          $dom.addEventListener('touchmove', moving)
+        }
+      }, 100)
+
+      $dom.addEventListener(
+        'touchend',
+        () => {
+          id && clearInterval(id)
+          $dom.removeEventListener('touchmove', moving)
+          player.seek(minmax(player.currentTime + shouldSeekSec, [0, player.duration]))
+          touchStartPointerX = shouldSeekSec = touchedTime = 0
+        },
+        { once: true }
+      )
+    })
+  }
+
   $.render($dom, el)
+}
+
+function minmax(value: number, range: [number, number]) {
+  return Math.min(Math.max(value, range[0]), range[1])
 }
 
 export default render
