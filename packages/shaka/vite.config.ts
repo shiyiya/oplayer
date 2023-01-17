72020eac6e080abbd0e03d29d27f1e25a455e1fe
shiyiya <shiyiya.11@gmail.com>
2023-01-17 08:52:58 +0800

    remove shaka

--- packages/shaka/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { LibraryOptions } from 'vite'
-import { viteConfig } from '../../vite.config'
-
-export default viteConfig('shaka', {
-  build: {
-    lib: { name: 'OShaka' } as LibraryOptions
-  }
-})
