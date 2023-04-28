19b7006ffa66fa1bdc9c8dfa7e7effaaf0551261
shiyiya <shiyiya.11@gmail.com>
2023-04-28 15:02:35 +0800

    fix typo

--- packages/ui/src/functions/contextmenu.ts
+++ packages/ui/src/functions/contextmenu.ts
@@ -2,7 +2,7 @@ import type { Player } from '@oplayer/core'
 import { DATA_CONTEXTMENU_ATTR_NAME } from '../style'
 
 export default function registerContextMenu(player: Player) {
-  player.$root.addEventListener('contextmenu', () => {
+  player.on('contextmenu', () => {
     player.$root.setAttribute(
       DATA_CONTEXTMENU_ATTR_NAME,
       `${!Boolean(player.$root.getAttribute(DATA_CONTEXTMENU_ATTR_NAME))}`
