591de88223b6e7b9919958595885c9f8b9a2c16d
CY <shiyiya.11@gmail.com>
2024-10-18 09:34:00 +0000

    Closed #140

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -12,124 +12,110 @@ import {
   useRef
 } from 'react'
 
-export interface ReactOPlayerProps extends PlayerOptions {
+export interface ReactOPlayerProps extends Omit<PlayerOptions, 'source'> {
   playing?: boolean
   duration?: number
   aspectRatio?: number
   plugins?: PlayerPlugin[]
   onEvent?: (e: PlayerEvent) => void
+  source?: Source | Promise<Source>
 }
 
-const ReactOPlayer = forwardRef(
-  (props: ReactOPlayerProps & { source?: Source | Promise<Source> }, ref: Ref<Player | null>) => {
-    const { playing, duration, aspectRatio = 9 / 16, plugins = [], onEvent, ...rest } = props
+const ReactOPlayer = forwardRef((props: ReactOPlayerProps, ref: Ref<Player | null>) => {
+  const { playing, duration, aspectRatio = 9 / 16, plugins = [], onEvent, source, ...rest } = props
 
-    const onEventRef = useRef(onEvent)
-    onEventRef.current = onEvent
+  const onEventRef = useRef(onEvent)
+  onEventRef.current = onEvent
 
-    const player = useRef<Player | null>(null)
-    const preSource = usePrevious(rest.source)
+  const player = useRef<Player | null>(null)
+  const preSource = usePrevious(source)
 
-    const isReady = player.current && player.current.$root
+  const isReady = player.current && player.current.$root
 
-    const onRefChange = useCallback((node: HTMLDivElement) => {
-      if (node !== null && !isReady) {
-        player.current = Player.make(node, rest).use(plugins).create()
-        if (typeof duration == 'number') player.current.seek(duration / 1000)
-        if (onEvent) {
-          player.current.on((payload: PlayerEvent) => onEventRef.current?.(payload))
-        }
+  const onRefChange = useCallback((node: HTMLDivElement) => {
+    if (node !== null && !isReady) {
+      player.current = Player.make(node, rest).use(plugins).create()
+      if (source instanceof Promise || (source as Source).src) player.current.changeSource(source!)
+      if (typeof duration == 'number') player.current.seek(duration / 1000)
+      if (onEvent) {
+        player.current.on((payload: PlayerEvent) => onEventRef.current?.(payload))
       }
-    }, [])
+    }
+  }, [])
 
-    useEffectIf(
-      isReady,
-      () => {
-        if (playing) {
-          if (!player.current!.isPlaying) player.current!.play()
-        } else {
-          if (player.current!.isPlaying) player.current!.pause()
-        }
-      },
-      [playing]
-    )
+  useEffectIf(isReady, () => {
+    if (playing) {
+      if (!player.current!.isPlaying) player.current!.play()
+    } else {
+      if (player.current!.isPlaying) player.current!.pause()
+    }
+  }, [playing])
 
-    useEffectIf(
-      isReady,
-      () => {
-        if (
-          (rest.source instanceof Promise && preSource != rest.source) ||
-          (rest.source?.src && preSource?.src !== rest.source.src)
-        ) {
-          player.current!.changeSource(rest.source)
-        }
-      },
-      [rest.source]
-    )
+  useEffectIf(isReady, () => {
+    if (
+      source &&
+      (source instanceof Promise
+        ? preSource != source
+        : source.src && (preSource as Source)?.src !== source.src)
+    ) {
+      player.current!.changeSource(source)
+    }
+  }, [source])
 
-    useEffectIf(
-      isReady && typeof duration === 'number',
-      () => {
-        player.current!.seek(duration! / 1000)
-      },
-      [duration]
-    )
+  useEffectIf(
+    isReady && typeof duration === 'number',
+    () => {
+      player.current!.seek(duration! / 1000)
+    },
+    [duration]
+  )
 
-    useEffectIf(
-      isReady,
-      () => {
-        if (rest.muted) {
-          player.current!.mute()
-        } else {
-          player.current!.unmute()
-        }
-      },
-      [rest.muted]
-    )
+  useEffectIf(isReady, () => {
+    if (rest.muted) {
+      player.current!.mute()
+    } else {
+      player.current!.unmute()
+    }
+  }, [rest.muted])
 
-    useEffectIf(
-      isReady,
-      () => {
-        player.current!.setPlaybackRate(rest.playbackRate!)
-      },
-      [rest.playbackRate]
-    )
+  useEffectIf(isReady, () => {
+    player.current!.setPlaybackRate(rest.playbackRate!)
+  }, [rest.playbackRate])
 
-    useEffect(() => {
-      return () => player.current?.destroy()
-    }, [])
+  useEffect(() => {
+    return () => player.current?.destroy()
+  }, [])
 
-    useImperativeHandle(ref, () => player.current, [])
+  useImperativeHandle(ref, () => player.current, [])
 
-    return useMemo(() => {
-      if (aspectRatio == 0) {
-        return <div ref={onRefChange}></div>
-      }
+  return useMemo(() => {
+    if (aspectRatio == 0) {
+      return <div ref={onRefChange}></div>
+    }
 
-      return (
+    return (
+      <div
+        style={{
+          width: '100%',
+          paddingTop: `${aspectRatio * 100}%`,
+          backgroundColor: '#f4f4f4',
+          position: 'relative'
+        }}
+      >
         <div
           style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
             width: '100%',
-            paddingTop: `${aspectRatio * 100}%`,
-            backgroundColor: '#f4f4f4',
-            position: 'relative'
+            height: '100%'
           }}
-        >
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%'
-            }}
-            ref={onRefChange}
-          ></div>
-        </div>
-      )
-    }, [])
-  }
-)
+          ref={onRefChange}
+        ></div>
+      </div>
+    )
+  }, [])
+})
 
 const useEffectIf = (where: any, cb: EffectCallback, deps?: DependencyList): void => {
   useEffect(() => {
